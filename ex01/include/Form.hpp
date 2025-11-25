7cffd83bc28223a9a02bef8fb8dd53214e26e207
Ale-Seme <alexei.semeraro@gmail.com>
2025-11-25 19:23:36 +0100

    created form class template

---
+++ ex01/include/Form.hpp
@@ -0,0 +1,35 @@
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <iostream>
+
+
+class Form {
+
+    private:
+        std::string _name;
+        bool _singed;
+        const int _gradeToSign;
+        const int _gradeToExecute;
+
+    public:
+        Form();
+        Form(std::string name, bool isSigned, int gradeToSign, int gradeToExecute);
+        Form(const Form &other);
+        Form& operator=(const Form &other);
+        ~Form();
+
+        class GradeTooHighException : public std::exception {
+            const char* what() const noexcept override;
+
+        };
+        class GradeTooLowException : public std::exception {
+            const char* what() const noexcept override;
+        };
+
+};
+
+std::ostream& operator<<(std::ostream& out, const &Form);
+
+
+#endif
